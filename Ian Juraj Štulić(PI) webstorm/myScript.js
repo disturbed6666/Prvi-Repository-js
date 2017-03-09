88b8d6484a1d968e698e42dd3f69834130435ab5
disturbed6666 <ianj3000@gmail.com>
2017-03-09 09:16:05 +0100

    Add files via upload

---
+++ Ian Juraj Štulić(PI) webstorm/myScript.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Ucenik on 23.2.2017..
+ */
+function myFunction() {
+    document.getElementById("demo").innerHTML = "<h1>Paragraph changed.</h1>";
+    alert("Ovdje ide poruka koja će se prikazati u prozoru");
+    confirm("Molimo odaberite");
+    prompt("Molimo unesite Vaše ime","Luke Skywalker");
+    var x = confirm("Molimo odaberite");
+    alert(x);
+};
+document.write("Dobar dan!");
+document.write("<h1>Dobar dan!</h1><p>Želim vam sretno s JavaScript-om!</p>");
+
